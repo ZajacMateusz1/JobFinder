2d5d449ef7f44e5a9c7176224be381e684d77a99
Mateusz Zajac <144244766+ZajacMateusz1@users.noreply.github.com>
2026-09-12 13:34:27 +0200

    Change save_preferences method in PreferencesRepository

--- src/modules/preferences/repository.py
+++ src/modules/preferences/repository.py
@@ -1,6 +1,7 @@
+from sqlalchemy import select
 from sqlalchemy.orm import Session
 from src.modules.ai.schemas import AnalyzeCvAiResponse
-from src.db.models import Preferences
+from src.db.models import Preferences, Skills
 from .schemas import PreferencesRequest
 
 
@@ -13,18 +14,20 @@ class PreferencesRepository:
         preferences_data: AnalyzeCvAiResponse | PreferencesRequest,
         user_id: str,
     ):
+        stmt = select(Skills).where(Skills.name.in_(preferences_data.skills))
+        skills = self._db.scalars(stmt).all()
         preferences = Preferences(
             location=preferences_data.location,
             min_salary=preferences_data.min_salary,
             max_salary=preferences_data.max_salary,
             remote_work=preferences_data.remote_work,
             user_id=int(user_id),
-            skills=preferences_data.skills,
+            skills=skills,
             experience_level=preferences_data.experience_level,
         )
         self._db.add(preferences)
         self._db.commit()
-        self._db.refresh()
+        self._db.refresh(preferences)
         return preferences
 
     def change_preferences(self, new_preferences: PreferencesRequest, user_id: str):
--- src/modules/preferences/service.py
+++ src/modules/preferences/service.py
@@ -2,7 +2,7 @@ from fastapi import UploadFile
 from .repository import PreferencesRepository
 from src.modules.ai.service import AiService
 from .utils import validate_cv_file
-from .schemas import CreatePreferencesRequest
+from .schemas import PreferencesRequest
 
 
 class PreferencesService:
@@ -15,12 +15,17 @@ class PreferencesService:
         cv_content = await cv_file.read()
         ai_result = self._ai_service.analyze_cv(cv_content=cv_content)
         analyze_cv_response = self._repository.save_preferences(
-            ai_result=ai_result, user_id=user_id
+            preferences_data=ai_result, user_id=user_id
         )
         return analyze_cv_response
 
-    def create_preferences(self, preferences: CreatePreferencesRequest, user_id: str):
+    def create_preferences(self, preferences: PreferencesRequest, user_id: str):
         create_preferences_response = self._repository.save_preferences(
             preferences, user_id
         )
         return create_preferences_response
+
+    def change_preferences(self, new_preferences: PreferencesRequest, user_id: str):
+        self.change_preferences_response = self._repository.change_preferences(
+            new_preferences=new_preferences, user_id=user_id
+        )
