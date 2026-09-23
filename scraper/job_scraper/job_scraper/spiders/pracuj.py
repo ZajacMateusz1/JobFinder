c1da639323ae3c0ad15eaf4326d2e12aa8ed293b
Mateusz Zajac <144244766+ZajacMateusz1@users.noreply.github.com>
2026-09-23 22:10:16 +0200

    Add basic PracujSpider

--- scraper/job_scraper/job_scraper/spiders/pracuj.py
+++ scraper/job_scraper/job_scraper/spiders/pracuj.py
@@ -1,10 +1,17 @@
+import json
 import scrapy
 
 
 class PracujSpider(scrapy.Spider):
     name = "pracuj"
     allowed_domains = ["pracuj.pl"]
-    start_urls = ["https://www.pracuj.pl/"]
+    start_urls = ["https://it.pracuj.pl/praca/warszawa;wp?rd=30&et=1%2C17&itth=33%2C37"]
 
     def parse(self, response):
-        print(response.text)
+        data = response.css("script#__NEXT_DATA__::text").get()
+        parsed_data = json.loads(data)
+        offers = parsed_data["props"]["pageProps"]["dehydratedState"]["queries"][0][
+            "state"
+        ]["data"]["groupedOffers"]
+
+        print(offers)
